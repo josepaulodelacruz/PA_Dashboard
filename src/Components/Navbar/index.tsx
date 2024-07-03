34e6e97e4854c5ff17aa42493effc6e0bc8338ba
PApropertiesJose <jmdelacruz@paproperties.com.ph>
2024-07-03 14:17:51 +0800

    navbar passing props RouteModel

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,11 +1,13 @@
 import Paper from '@mui/material/Paper'
 import HomeIcon from '@mui/icons-material/Home'
+import TablesIcon from '@mui/icons-material/TableRows'
 import ProfileIcon from '@mui/icons-material/Person'
 import SettingIcon from '@mui/icons-material/Settings'
 import NotificationIcon from '@mui/icons-material/NotificationImportant'
 import { useTheme } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
+import { RouteModel } from '@/Types'
 
 
 /**
@@ -13,40 +15,54 @@ import Box from '@mui/material/Box'
 **/
 
 interface NavbarProps {
-  isScrolled: boolean
+  isScrolled: boolean,
+  route: RouteModel
 }
 
-const Navbar = ({ isScrolled } : NavbarProps) => {
+const Navbar = ({ isScrolled, route }: NavbarProps) => {
   const theme = useTheme()
-
 
   let colorIcon = theme.palette.grey[500]
   let backgroundColor = isScrolled ? 'rgba(255, 255, 255, 0.8)' : 'transparent';
   let backdropFilter = isScrolled ? 'saturate(200%) blur(1.875rem);' : 'none';
   let boxShadow = isScrolled ? 'rgba(255, 255, 255, 0.9) 0rem 0rem 0.0625rem 0.0625rem inset, rgba(0, 0, 0, 0.05) 0rem 1.25rem 1.6875rem 0rem' : 'none'
 
+  let IconComponent = function() {
+    if(route.name === 'Dashboard') {
+      return <HomeIcon fontSize='small' style={{ color: colorIcon, fontSize: '16px' }} />
+    } else {
+      return <TablesIcon fontSize='small' style={{ color: colorIcon, fontSize: '16px' }} />
+    }
+  }
+
+
   return (
     <Paper
       sx={{
-      position: 'sticky',
-      top: '1rem',
-      zIndex: 1100,
-      boxShadow: boxShadow,
-      marginTop: '2rem', marginRight: '1rem', marginLeft: '1rem', paddingTop: '0.5rem', paddingBottom: '0.5rem',
-      background: backgroundColor,
-      backdropFilter: backdropFilter,
-    }} elevation={0}>
+        transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+        position: 'sticky',
+        top: '1rem',
+        zIndex: 1100,
+        boxShadow: boxShadow,
+        marginTop: '2rem',
+        marginRight: '1rem',
+        marginLeft: '1rem',
+        paddingTop: '0.5rem',
+        paddingBottom: '0.5rem',
+        background: backgroundColor,
+        backdropFilter: backdropFilter,
+      }} elevation={0}>
       <nav className='flex flex-row px-4 justify-between items-center'>
 
         <div className='flex flex-col'>
           <div className='flex flex-row items-center'>
-            <HomeIcon fontSize='small' style={{ color: colorIcon, fontSize: '16px' }} />
+            <IconComponent />
             <div className='text-sm' style={{ color: '#344767' }}>
               <span className='px-2'>/</span>
-              <span>Dashboard</span>
+              <span>{route.name}</span>
             </div >
           </div>
-          <span className='font-bold text-[1rem] ' style={{ color: '#344767', lineHeight: '1.625' }}>Dashboard</span>
+          <span className='font-bold text-[1rem] ' style={{ color: '#344767', lineHeight: '1.625' }}>{route.name}</span>
         </div>
 
         <div className='flex flex-row items-center'>
--- src/Layouts/TemplateContainer/index.tsx
+++ src/Layouts/TemplateContainer/index.tsx
@@ -1,12 +1,16 @@
 import Sidenav from "@/Components/Sidenav"
 import Grid from "@mui/material/Grid"
 import Navbar from '@/Components/Navbar'
-import { Outlet } from 'react-router-dom'
-import { useState, useEffect, useRef } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useState, useEffect, useRef, useLayoutEffect } from 'react'
+import { routes } from '@/routes'
+import { RouteModel } from "@/Types"
 
 const TemplateContainer = () => {
+  const location = useLocation()
   const [isScrolled, setIsScrolled] = useState<boolean>(false)
   const scrollableRef = useRef<any>(null);
+  const [routeObject, setRouteObject] = useState<RouteModel>(routes[0]);
 
   const handleScroll = () => {
     if (scrollableRef.current) {
@@ -16,7 +20,22 @@ const TemplateContainer = () => {
         setIsScrolled(false);
       }
     }
-  };
+  }
+
+  useLayoutEffect(() => {
+    let matchedRoute = null;
+    for (let i = 0; i < routes.length; i++) {
+      if (routes[i].route === location.pathname) {
+        matchedRoute = routes[i];
+        break;
+      }
+    }
+
+    if (matchedRoute) {
+      setRouteObject(matchedRoute);
+    }
+
+  }, [location.pathname, routeObject])
 
   useEffect(() => {
     const scrollableElement = scrollableRef.current;
@@ -35,7 +54,7 @@ const TemplateContainer = () => {
       </Grid>
 
       <Grid ref={scrollableRef} item md={10} xs={12} style={{ height: '100vh', overflowY: 'auto' }} >
-        <Navbar isScrolled={isScrolled} />
+        <Navbar isScrolled={isScrolled} route={routeObject}/>
         <Outlet />
       </Grid>
 
