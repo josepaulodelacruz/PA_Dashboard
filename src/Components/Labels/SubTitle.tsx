c6a5f694b9a6eb4e4df7fc145b15423f9dc82e65
jpeeicruz <jp.delacruz@kabootekph.com>
2024-07-04 22:13:41 +0800

    no message

--- src/Components/Labels/SubTitle.tsx
+++ src/Components/Labels/SubTitle.tsx
@@ -8,7 +8,6 @@ interface SubTitleLabelProps {
 const SubTitleLabel = ({ children } : SubTitleLabelProps) => {
   const theme = useTheme();
   const { text } = theme.palette as { text?: any }
-  console.log(text.main)
 
   return (
     <div className="text-xs font-bold" style={{color: text.main}}>
