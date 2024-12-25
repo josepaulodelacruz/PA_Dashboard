4a3cedffcb3cce0f9ed714c7be06d7c1797f65c8
josepaulodelacruz <whoseepowlu@gmail.com>
2024-12-25 11:22:03 +0800

    fix: change color:

--- src/Pages/Map/index.tsx
+++ src/Pages/Map/index.tsx
@@ -8,12 +8,12 @@ const MapPage = () => {
 
       <div className="h-[500px] relative w-full flex justify-center mb-10">
 
-        <div className='h-[93%] shadow-xl relative top-[-70] z-10 rounded-xl w-[98%]' style={{ background: 'blue'}}>
+        <div className='h-[93%] shadow-md relative top-[-70] z-10 rounded-xl w-[98%] bg-yellow-50' >
         </div>
         <Paper className="absolute px-7 pb-5 items-start mt-10 bg-white flex flex-col 
           justify-end w-full h-full top-0 left-0 rounded-xl shadow-md"
           sx={{ borderRadius: '0.75rem', boxShadow: 'rgba(0, 0, 0, 0.1) 0rem 0.25rem 0.375rem -0.0625rem, rgba(0, 0, 0, 0.06) 0rem 0.125rem 0.25rem -0.0625rem' }}>
-          <MainSpan>Title</MainSpan>
+          <MainSpan>MAP</MainSpan>
           <SubSpan>Sub Title</SubSpan>
         </Paper>
 
