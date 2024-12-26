14d9eb0ebff9a096461fba07ef889bac1784320b
josepaulodelacruz <whoseepowlu@gmail.com>
2024-12-26 15:55:36 +0800

    no message

--- src/Pages/Map/index.tsx
+++ src/Pages/Map/index.tsx
@@ -1,26 +1,21 @@
-import { MainSpan, SubSpan } from "@/Components/Labels/Spans";
 import DashboardLayout from "@/Layouts/DashboardLayout";
-import { Paper } from "@mui/material";
 
 const MapPage = () => {
   return (
     <DashboardLayout>
 
-      <div className="h-[500px] relative w-full flex justify-center mb-10">
+      <div className="block bg-white mt-8 rounded-md pb-10 shadow-sm">
 
-        <div className='h-[93%] shadow-md relative top-[-70] z-10 rounded-xl w-[98%] bg-yellow-50' >
+        <div className="relative h-[600px] bg-yellow-100 flex flex-grow shadow-md ml-4 mr-4 mt-4 top-[-40px] rounded-lg" >
+
+
         </div>
-        <Paper className="absolute px-7 pb-5 items-start mt-10 bg-white flex flex-col 
-          justify-end w-full h-full top-0 left-0 rounded-xl shadow-md"
-          sx={{ borderRadius: '0.75rem', boxShadow: 'rgba(0, 0, 0, 0.1) 0rem 0.25rem 0.375rem -0.0625rem, rgba(0, 0, 0, 0.06) 0rem 0.125rem 0.25rem -0.0625rem' }}>
-          <MainSpan>MAP</MainSpan>
-          <SubSpan>Sub Title</SubSpan>
-        </Paper>
+
+        <section className="px-4 relative top-[-20px]">
+
+        </section>
 
       </div>
-
-
-
     </DashboardLayout>
 
   )
